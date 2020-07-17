ef8b4f77a124ccc768435ad8eb603645045e5737
cl4udino <lhcs10@gmail.com>
2020-07-17 17:16:42 -0300

    Modificando settings

--- errorcenter/settings.py
+++ errorcenter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=lcw6prb&sjks4*_#augji@st57=sr1c&*6e!040v&+tnj&m8@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
